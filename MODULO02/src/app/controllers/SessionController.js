737507dc52d284f2f2157ea60db059ace8dde401
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-01-30 23:03:25 -0300

    Add generate token JWT

---
+++ MODULO02/src/app/controllers/SessionController.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      res.status(401).json({ error: 'Usuário não encontrado' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      res.status(401).json({ error: 'Senha incorreta' });
+    }
+
+    const { id, name } = user;
+
+    res.json({
+      id,
+      name,
+      email,
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
