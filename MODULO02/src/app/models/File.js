dd3ac1de77e25f1044e778e0359f6beafb421b63
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-02-03 23:09:05 -0300

    Add provider controller

--- MODULO02/src/app/models/File.js
+++ MODULO02/src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.INTEGER,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3000/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
--- MODULO02/src/routes.js
+++ MODULO02/src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
@@ -18,5 +19,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/uploads', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
