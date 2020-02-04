0cee4ccb4f4e8922866b0e2c97b39f0787e59aaa
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-02-03 22:26:03 -0300

    create upload files + model file (avatar)

--- MODULO02/src/routes.js
+++ MODULO02/src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
+import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -11,5 +17,6 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/uploads', upload.single('file'), FileController.store);
 
 export default routes;
