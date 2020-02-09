e268b407908a37ec2b85b5a944adf4910629cbdf
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-02-09 12:22:07 -0300

    Sentry config + environment variables + exception handler

--- MODULO02/src/app.js
+++ MODULO02/src/app.js
@@ -1,17 +1,25 @@
+import 'dotenv/config';
 import express from 'express';
 import { resolve } from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
 class App {
   constructor() {
+    Sentry.init(sentryConfig);
     this.server = express();
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,7 +28,15 @@ class App {
   }
 
   routes() {
+    this.server.use(Sentry.Handlers.errorHandler());
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
--- MODULO02/src/config/auth.js
+++ MODULO02/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'f6698fc96e0b3a19056a1de6eabeeb82',
+  secret: process.env.APP_SECRET,
   expiresIn: '3d',
 };
