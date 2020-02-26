cf703ac01fe4505931349ed71dc1a5930213cd0d
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-02-26 10:56:58 -0300

    Initial commit of React Native :fire:

---
+++ MODULO06/src/pages/User/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function User() {
+  return (
+    <View />
+  );
+}
+
+User.navigationOptions = {
+  title: 'Usuários'
+};
