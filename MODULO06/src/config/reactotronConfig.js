012c1e783aa8bcdd2340c8506d30bdd553c7b30b
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-02-29 18:36:50 -0300

    Update users page + api github

--- MODULO06/src/config/reactotronConfig.js
+++ MODULO06/src/config/reactotronConfig.js
@@ -1,9 +1,13 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-    const tron = Reactotron.configure().useReactNative().connect();
+  const tron = Reactotron.configure({
+    enabled: true,
+    host: '192.168.15.10',
+    port: 9090
+  }).useReactNative().connect();
 
-    console.tron = tron;
+  console.tron = tron;
 
-    tron.clear();
+  tron.clear();
 }
--- MODULO06/src/pages/Main/index.js
+++ MODULO06/src/pages/Main/index.js
@@ -1,11 +1,54 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Container } from './styles';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
 
-export default function Main() {
-  return (
-    <Container />
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: []
+  }
+
+  handleNewUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      avatar: response.data.avatar,
+      bio: response.data.bio
+    }
+
+    this.setState({ users: [...users, data], newUser: '' });
+
+    Keyboard.dismiss();
+  }
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite o nome do usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleNewUser}
+          />
+          <SubmitButton onPress={this.handleNewUser}>
+            <Icon name="add" size={20} color='#FFF' />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
