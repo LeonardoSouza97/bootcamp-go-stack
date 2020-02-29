7377c287a01ad71993b267cb2b17158fb1dd2403
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-02-29 19:49:54 -0300

    Add list users + loading button

--- MODULO06/src/pages/Main/index.js
+++ MODULO06/src/pages/Main/index.js
@@ -1,34 +1,54 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native'
+import { Keyboard, ActivityIndicator } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container, Form, Input, SubmitButton, List, User, Avatar, Name, Bio, ProfileButton, ProfileButtonText } from './styles';
 import api from '../../services/api';
 
 export default class Main extends Component {
   state = {
     newUser: '',
-    users: []
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users != users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
   }
 
   handleNewUser = async () => {
     const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
     const data = {
       name: response.data.name,
       login: response.data.login,
-      avatar: response.data.avatar,
+      avatar: response.data.avatar_url,
       bio: response.data.bio
     }
 
-    this.setState({ users: [...users, data], newUser: '' });
+    this.setState({ users: [...users, data], newUser: '', loading: false });
 
     Keyboard.dismiss();
   }
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
 
     return (
       <Container>
@@ -42,10 +62,27 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleNewUser}
           />
-          <SubmitButton onPress={this.handleNewUser}>
-            <Icon name="add" size={20} color='#FFF' />
+          <SubmitButton loading={loading} onPress={this.handleNewUser}>
+            {loading ? <ActivityIndicator color="#FFF"></ActivityIndicator> : <Icon name="add" size={20} color='#FFF' />}
           </SubmitButton>
         </Form>
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              <ProfileButton>
+                <ProfileButtonText>
+                  Ver perfil
+                </ProfileButtonText>
+              </ProfileButton>
+              <Avatar source={item.avatar} />
+            </User>
+          )}
+        />
       </Container>
     );
   }
--- MODULO06/src/pages/Main/styles.js
+++ MODULO06/src/pages/Main/styles.js
@@ -33,5 +33,59 @@ export const SubmitButton = styled(RectButton)`
   margin-left: 10px;
   padding: 0 12px;
   color: #EEE;
+  opacity: ${props => props.loading ? 0.7 : 1}
 `;
 
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 10px;
+`;
+
+export const Name = styled.Text`
+  font-size: 16px;
+  text-align: center;
+  color: #333;
+  font-weight: bold;
+  margin-top:4px;
+`;
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  background: #EEE;
+`;
+
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  text-align: center;
+  margin-bottom: 4px;
+  line-height: 18px;
+  font-size: 13px;
+  color: #999;
+  margin-top: 5px;
+`;
+
+export const ProfileButton = styled(RectButton)`
+ align-self: stretch;
+ align-items: center;
+ justify-content: center;
+ border-radius: 4px;
+ height: 36px;
+ margin: 10px;
+ background: #7159c1;
+`;
+
+export const ProfileButtonText = styled.Text`
+ text-transform: uppercase;
+ color: #FFF;
+ font-weight: bold;
+ font-size: 12px;
+`;
