4076e35e8a97f9ba624c919831446e30483ec8e0
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-04-01 21:55:39 -0300

    add reactotron-redux-saga + config

--- MODULO07/src/store/index.js
+++ MODULO07/src/store/index.js
@@ -4,7 +4,9 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './models/rootReducer';
 import rootSaga from './models/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
+const sagaMiddleware = createSagaMiddleware({sagaMonitor});
 
 const enhancer = process.env.NODE_ENV === 'development' ?
   compose(console.tron.createEnhancer(), applyMiddleware(sagaMiddleware)) : applyMiddleware(sagaMiddleware);
