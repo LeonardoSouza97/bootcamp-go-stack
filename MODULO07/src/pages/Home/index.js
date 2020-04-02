b19c595065181d6b434307b0326f3cb067a2fd0b
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2020-04-01 21:46:21 -0300

    add redux-saga

--- MODULO07/src/pages/Home/index.js
+++ MODULO07/src/pages/Home/index.js
@@ -28,10 +28,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   }
 
   render() {
@@ -47,7 +47,7 @@ class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormmated}</span>
 
-            <button type='button' onClick={() => this.handleAddProduct(product)}>
+            <button type='button' onClick={() => this.handleAddProduct(product.id)}>
               <div>
                 <MdShoppingBasket size={16} color="#FFF" /> {amount[product.id] || 0}
               </div>
